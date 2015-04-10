fc17e795c24444fae0459d0e5f1640b7bf934241
LiaKitamura <kit4mura@gmail.com>
2015-04-09 19:57:41 -0600

    creating a table

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,27 +18,27 @@
 
 $(document).ready(function(){
 
-  // Katarina Mills. Id: 4
-  var $webSkillsNewHeight = $('.Katarina').height();
-  $('._4').height($webSkillsNewHeight);
-
-  // Miss Cortez Hilpert. Id: 3
-  var $webSkillsNewHeight = $('.Miss').height();
-  $('._3').height($webSkillsNewHeight);
-
-  // Dakota Kautzer. Id: 2
-  var $webSkillsNewHeight = $('.Dakota').height();
-  $('._2').height($webSkillsNewHeight);
-
-  // Russ Waters. Id: 1
-  var $webSkillsNewHeight = $('.Russ').height();
-  $('._1').height($webSkillsNewHeight);
-
-
-  var $designerSkillsNewHeight = $('.Mikel').height();
-  $('._8').height($designerSkillsNewHeight);
-
-
+  // // Katarina Mills. Id: 4
+  // var $webSkillsNewHeight = $('.Katarina').height();
+  // $('._4').height($webSkillsNewHeight);
+  //
+  // // Miss Cortez Hilpert. Id: 3
+  // var $webSkillsNewHeight = $('.Miss').height();
+  // $('._3').height($webSkillsNewHeight);
+  //
+  // // Dakota Kautzer. Id: 2
+  // var $webSkillsNewHeight = $('.Dakota').height();
+  // $('._2').height($webSkillsNewHeight);
+  //
+  // // Russ Waters. Id: 1
+  // var $webSkillsNewHeight = $('.Russ').height();
+  // $('._1').height($webSkillsNewHeight);
+  //
+  //
+  // var $designerSkillsNewHeight = $('.Mikel').height();
+  // $('._8').height($designerSkillsNewHeight);
+  //
+  
   // sets web developer job height to the same height of the web developer applicant names
   var $webNewHeight = $('.web_dev_names').height();
   $('.Web').height($webNewHeight);
