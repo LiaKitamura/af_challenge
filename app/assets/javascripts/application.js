2f7e3133f75758e8cb1f471ad2ac694d207670e8
LiaKitamura <kit4mura@gmail.com>
2015-04-14 17:02:34 -0600

    deleted some comments. added more borders

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,32 +18,18 @@
 
 $(document).ready(function(){
 
-  // // Katarina Mills. Id: 4
-  // var $webSkillsNewHeight = $('.Katarina').height();
-  // $('._4').height($webSkillsNewHeight);
   var $webNewHeight = $('.Katarina').height();
   $('._4').height($webNewHeight);
 
-  // // Miss Cortez Hilpert. Id: 3
-  // var $webSkillsNewHeight = $('.Miss').height();
-  // $('._3').height($webSkillsNewHeight);
   var $webNewHeight = $('.Miss').height();
   $('._3').height($webNewHeight);
 
-  // // Dakota Kautzer. Id: 2
-  // var $webSkillsNewHeight = $('.Dakota').height();
-  // $('._2').height($webSkillsNewHeight);
   var $webNewHeight = $('.Dakota').height();
   $('._2').height($webNewHeight);
 
-  // // Russ Waters. Id: 1
-  // var $webSkillsNewHeight = $('.Russ').height();
-  // $('._1').height($webSkillsNewHeight);
   var $webNewHeight = $('.Russ').height();
   $('._1').height($webNewHeight);
 
-  // var $designerSkillsNewHeight = $('.Mikel').height();
-  // $('._8').height($designerSkillsNewHeight);
   var $designerNewHeight = $('.Mikel').height();
   $('._8').height($designerNewHeight);
 
@@ -57,15 +43,9 @@ $(document).ready(function(){
   $('._5').height($designerNewHeight);
 
 
-  // sets web developer job height to the same height of the web developer applicant names
-  // var $webNewHeight = $('.web_dev_names').height();
-  // $('.Web').height($webNewHeight);
   var $webNewHeight = $('.cover_letters').height();
   $('.Web').height($webNewHeight);
 
-  // sets designer job height to the same height of the designer applicant names
-  // var $designerNewHeight = $('.designer_names').height();
-  // $('.Designer').height($designerNewHeight);
   var $designerNewHeight = $('.cover_letters').height();
   $('.Designer').height($designerNewHeight);
 
